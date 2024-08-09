0fa04573cc68811591d4b1d3237777439f7a0ad2
yoonhyeyeon <163373010+yoonhyeyeon@users.noreply.github.com>
2024-08-09 13:09:12 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -3,14 +3,17 @@ class Solution {
     	// 길이가 1 더 긴 새 배열 생성
         int[] answer = new int[num_list.length + 1];
         // 기존 배열의 모든 요소를 새 배열로 복사
-        System.arraycopy(num_list, 0, answer, 0, num_list.length);
+//        System.arraycopy(num_list, 0, answer, 0, num_list.length);
+        for(int i = 0; i < num_list.length; i++) {
+        	answer[i] = num_list[i];
+        }
         
     	// 마지막원소 > 그전 원소 = 마지막원소 - 그전 원소
     	if( num_list[num_list.length-1] > num_list[num_list.length - 2] ) {
     		// 새로운 요소 추가
-    		answer[answer.length - 1 ] = num_list[num_list.length- 1 ] - num_list[num_list.length- 2 ];
+    		answer[num_list.length] = num_list[num_list.length- 1 ] - num_list[num_list.length- 2 ];
     	}else{   // 마지막원소 < 그전 원소 = 2 * 마지막원소
-    		answer[answer.length - 1 ] = 2 * num_list[num_list.length - 1];
+    		answer[num_list.length] = num_list[num_list.length - 1] * 2;
     	}
         
         return answer;
