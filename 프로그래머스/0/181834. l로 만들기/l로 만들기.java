187e3bac47953480221884d3748c551b4a138d63
yoonhyeyeon <163373010+yoonhyeyeon@users.noreply.github.com>
2025-05-13 13:19:35 +0900

    [level 0] Title: l로 만들기, Time: 21.05 ms, Memory: 85.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String myString) {
+
+        String replacement = myString.replaceAll("[abcdefghijk]" , "l");
+        
+        return replacement;
+    }
+}
