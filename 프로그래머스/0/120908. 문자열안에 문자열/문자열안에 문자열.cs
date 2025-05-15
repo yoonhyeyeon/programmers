1f67bd2e945c639d6ab10d1a0fd2046f3ce2d3f2
yoonhyeyeon <163373010+yoonhyeyeon@users.noreply.github.com>
2025-05-15 15:00:36 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.23 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(string str1, string str2) {
+        int answer = 0;
+
+        if( str1.Contains(str2) ){
+            answer = 1;
+        }else{
+            answer = 2;
+        }
+        
+        return answer;
+    }
+}
