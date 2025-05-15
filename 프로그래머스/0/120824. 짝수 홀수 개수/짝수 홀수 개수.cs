1ad78673278da503f93f2069fad68f2ff53c5acd
yoonhyeyeon <163373010+yoonhyeyeon@users.noreply.github.com>
2025-05-15 15:57:51 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.26 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+
+        for( int i = 0; i < num_list.Length; i++){
+            if( num_list[i] % 2 == 0 ){
+                answer[0] ++;
+            }else{
+                answer[1] ++;
+            }
+        }
+        
+        return answer;
+    }
+}
